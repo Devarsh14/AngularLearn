2103a3211da541d5d9a18c887c961995b289284a
Devarsh shukla <Devarsh14@gmail.com>
2019-03-09 11:54:31 +1000

    constrocutor importane lesson for the TypeScript

--- src/app/favorite-color-component/favorite-color-component.component.ts
+++ src/app/favorite-color-component/favorite-color-component.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-favorite-color-component',
   templateUrl: './favorite-color-component.component.html',
-  styleUrls: ['./favorite-color-component.component.scss']
+  styleUrls: ['./favorite-color-component.component.scss'],
+  template : `Favorite Color: <input type = "text">
+  [formControl]= "favoriteColorControl"`
 })
 export class FavoriteColorComponentComponent implements OnInit {
 
-  constructor() { }
+  // Defination needs to be outside of constructor other wise throws error.
+  favoriteColorControl = new FormControl('');
+  dev;
+  constructor() {
+    dev = new FormControl();
+}
 
   ngOnInit() {
   }
